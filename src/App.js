c3358203b8064a3c7347e83c58c6ce46422100e0
Alexis Beaumont <alexisbeaumont97@gmail.com>
2023-11-14 21:28:48 +0100

    Stable 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import AddTodo from './AddTodo';
+import TodoList from './TodoList';
 import './App.css';
 
+const API_URL = 'https://laravel_todos_api.com/api/todos';
+
 function App() {
-  return (
-    <div className="App">
-      <AddTodo/>
-      <TodoList/>
-    </div>
-  );
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        axios.get(API_URL)
+            .then((response) => {
+                setTodos(response.data.todos);
+            });
+    }, []);
+
+    const deleteTodo = (todoId) => {
+        axios.post(`${API_URL}/delete`, { id: todoId })
+            .then(() => {
+                const newTodos = todos.filter((todo) => todo.id !== todoId);
+                setTodos(newTodos);
+            });
+    };
+
+    const completeTodo = (todoId) => {
+        axios.post(`${API_URL}/complete`, { id: todoId })
+            .then(() => {
+                const newTodos = todos.map((todo) => {
+                    if (todo.id === todoId) {
+                        return { ...todo, completed: !todo.completed };
+                    }
+                    return todo;
+                });
+                setTodos(newTodos);
+            });
+    };
+
+    const createTodo = (content) => {
+        axios.post(API_URL, { content })
+            .then((response) => {
+                setTodos([...todos, response.data.todo]);
+            });
+    };
+
+    return (
+        <div className="App">
+            <AddTodo createTodo={createTodo} />
+            <TodoList todos={todos} completeTodo={completeTodo} deleteTodo={deleteTodo} />
+        </div>
+    );
 }
 
 export default App;
